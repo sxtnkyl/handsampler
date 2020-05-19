701fee8da3a76d493552e5bfa1b605d471345938
sxtnkyl <sxtnkyl@gmail.com>
2020-05-19 13:03:28 -0400

    Updates

---
+++ precache-manifest.3b2105bc219710b3865cd38f8b2e2610.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd0da3b3104087447b394f22766c1dc6",
+    "url": "/handsampler/index.html"
+  },
+  {
+    "revision": "bfa355329031fc285e65",
+    "url": "/handsampler/static/css/main.f56537d3.chunk.css"
+  },
+  {
+    "revision": "dcafa06a1c344aa71c43",
+    "url": "/handsampler/static/js/2.459e0d9c.chunk.js"
+  },
+  {
+    "revision": "bfa355329031fc285e65",
+    "url": "/handsampler/static/js/main.9614761e.chunk.js"
+  },
+  {
+    "revision": "b38fa27ed53e23fa6cec",
+    "url": "/handsampler/static/js/runtime-main.ec3a5d73.js"
+  }
+]);
